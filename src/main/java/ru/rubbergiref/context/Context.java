3090b4e68fae5cc7ad90742ec41e99f1d22eb182
Ivan Churin <treleas2@gmail.com>
2018-08-28 18:45:56 +0300

    Parent functions

--- src/main/java/ru/rubbergiref/context/Context.java
+++ src/main/java/ru/rubbergiref/context/Context.java
@@ -7,19 +7,31 @@ import lombok.experimental.UtilityClass;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 @SuppressWarnings("unused")
 @UtilityClass
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Context {
-    private final Map<Class<?>, Object> SERVICES = new ConcurrentHashMap<Class<?>, Object>();
+    private final Map<Class<?>, Object> SERVICES = new ConcurrentHashMap<>();
+    private final Map<Class<?>, Function<Class<?>, ?>> PARENTS = new ConcurrentHashMap<>();
 
     /**
      * Get service provider
      */
     @SuppressWarnings("unchecked")
     public <T> T context(final Class<T> service) {
-        return (T) Context.SERVICES.get(service);
+        final T provider = (T) Context.SERVICES.get(service);
+        if (provider == null) {
+            for (final Function<Class<?>, ?> parent : Context.PARENTS.values()) {
+                final T parentProvider = (T) parent.apply(service);
+                if (parentProvider != null) {
+                    return parentProvider;
+                }
+            }
+        }
+
+        return provider;
     }
 
     /**
@@ -54,5 +66,26 @@ public class Context {
      */
     public boolean wrapped(final Class<?> service) {
         return Context.SERVICES.containsKey(service);
+    }
+
+    /**
+     * Register parent with get function
+     */
+    public <T> void warpParent(final Class<T> serviceManager, final Function<Class<?>, ?> parent) {
+        Context.PARENTS.put(serviceManager, parent);
+    }
+
+    /**
+     * Unregister parent
+     */
+    public <T> void unwrapParent(final Class<T> serviceManager) {
+        Context.PARENTS.remove(serviceManager);
+    }
+
+    /**
+     * All parents
+     */
+    public Set<Class<?>> parents() {
+        return Context.PARENTS.keySet();
     }
 }
