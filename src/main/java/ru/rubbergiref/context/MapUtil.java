e9ab111c948f747880689514ad0e53375ed1fed6
Ivan Churin <treleas2@gmail.com>
2019-05-28 09:01:00 +0300

    Fix fastutil maps

--- src/main/java/ru/rubbergiref/context/MapUtil.java
+++ src/main/java/ru/rubbergiref/context/MapUtil.java
@@ -3,28 +3,18 @@ package ru.rubbergiref.context;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @UtilityClass
 public class MapUtil {
-    private final boolean HAS_FAST_UTIL = ensureFastUtil();
-
-    public <A,B> Map<A, B> synchronizedMap() {
-        if (MapUtil.HAS_FAST_UTIL) {
-            return Object2ObjectMaps.synchronize(new Object2ObjectOpenHashMap<>());
-        }
-
-        return new ConcurrentHashMap<>();
-    }
-
-    private boolean ensureFastUtil() {
+    public <A,B> @NotNull Map<A, B> synchronizedMap() {
         try {
-            Class.forName("it.unimi.dsi.fastutil.objects.Object2ObjectMap");
-            return true;
-        } catch (final ClassNotFoundException e) {
-            return false;
+            return Object2ObjectMaps.synchronize(new Object2ObjectOpenHashMap<>());
+        } catch (final Exception ignored) {
+            return new ConcurrentHashMap<>();
         }
     }
 }
